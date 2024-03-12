bb26cd8b26f2d0f85609a4aba58887d4b1101b9d
Nima <sadeghinima090@gmail.com>
2024-03-13 00:50:01 +0330

    Hero section completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
-import './App.css'
+import "./App.css";
+import Shop from "./components/pages/shop";
+import Product from "./components/pages/product";
+import Login from "./components/pages/login";
+import Navbar from "./components/navbar/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App px[10%]">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Shop/>} />
+          <Route path="/login" element = {<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,61 @@
+import Logo from "../../Assets/Assets/logo.png";
+import cart_icon from "../../Assets/Assets/cart_icon.png";
+import { useState } from "react";
+import "./navbar.css";
+import { Outlet, Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [menu, setMneu] = useState("shop");
+  return (
+    <nav className=" w-full flex justify-between items-center bg-white text-black p-[16px] shadow-md ">
+      <div className="flex items-center justify-center mx-7">
+        <img className="w-[40px]" src={Logo} alt="" />
+        <h2 className="ml-2 text-xl font-bold">SHOPPER</h2>
+      </div>
+      <ul className="w-[30%] flex justify-between items-center font-semibold ">
+        <li
+          onClick={() => setMneu("shop")}
+          className={menu === "shop" ? "border-b-2 border-red-400 pb-1" : ""}
+        >
+             shop
+        </li>
+
+        <li
+          onClick={() => setMneu("men")}
+          className={menu === "men" ? "border-b-2 border-red-400 pb-1" : ""}
+        >
+          men
+        </li>
+        <li
+          onClick={() => setMneu("women")}
+          className={menu === "women" ? "border-b-2 border-red-400 pb-1" : ""}
+        >
+          women
+        </li>
+        <li
+          onClick={() => setMneu("kids")}
+          className={menu === "kids" ? "border-b-2 border-red-400 pb-1" : ""}
+        >
+          kids
+        </li>
+      </ul>
+      <div className="flex items-center justify-center mr-10">
+       
+        <button className="outline-none rounded-xl border-2	border-slate-300 bg-white  text-lg cursor-pointer font-semibold py-1 px-7 mr-6  hover:bg-slate-100 ">
+     
+          Login
+          
+        </button>
+       
+
+        <img src={cart_icon} alt="" />
+        <div className="w-[22px] h-[22px] flex justify-center items-center text-[14px] rounded-lg bg-red-500 text-white mt-[-20px] ml-[-15px]">
+          1
+        </div>
+      </div>
+      <Outlet />
+    </nav>
+  );
+};
+
+export default Navbar;
