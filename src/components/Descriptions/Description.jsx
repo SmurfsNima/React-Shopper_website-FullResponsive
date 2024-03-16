ac73718b9cebe4e3221e5a0ea23625a0029a9dc5
Nima <sadeghinima090@gmail.com>
2024-03-16 20:25:12 +0330

    Description ssection ended

---
+++ src/components/Descriptions/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import  './Description.css'
+const Description = () => {
+  return (
+    <div className='mt-[80px]'>
+        <div id='boxes' className='flex items-center ' >
+            <div className='font-semibold text'>Description</div>
+            <div className='bg-[#fbfbfb] text-[#555]'>Reviews (122)</div>
+        </div>
+        <div className=' flex flex-col  gap-6 p-[40px] pb-[70px] border-[1px] '>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos labore ipsum, a atque rerum possimus similique facilis ab eos quibusdam quis mollitia minus aperiam facere culpa enim laborum rem nobis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid numquam libero enim tenetur. Magnam, labore!</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Description
--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -2,13 +2,15 @@ import { useParams } from "react-router-dom";
 import all_product from "../../Assets/Assets/all_product";
 import BreadCrum from "../breadcrum/breadCrum";
 import ProductDisplay from "../ProductDisplay/ProductDisplay";
+import Description from "../Descriptions/Description";
 const Product = () => {
     const {productId} = useParams()
     const product = all_product.find((e)=>  e.id === Number(productId));
     console.log(product);
-    return ( <div className="h-[65vh] pl-[5%]">
+    return ( <div className=" pl-[5%]">
         <BreadCrum product={product} />
         <ProductDisplay product ={product} />
+        <Description />
         </div>
     );
 }
